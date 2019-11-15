03637559d509b46e5bf71a1394e4ef62c319ae33
Vitorgaps <vitorgabriel616@gmail.com>
2019-11-15 19:12:49 -0300

    Prototipo criado

---
+++ App.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <div id="app">
+     <section class="hero is-primary is-bold is-fullheight">
+      <!-- Hero head: will stick at the top -->
+      
+      <!-- Hero content: will be in the middle -->
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="row">
+            <div class="columns">
+              <div class="column is-half is-offset-one-quarter">
+                <todo-card></todo-card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+  </div>
+</template>
+
+<script>
+import TodoCard from './components/TodoCard'
+export default {
+  name: 'app',
+  components: {
+    TodoCard
+  },
+  data () {
+    return {
+      msg: 'Hello, World!'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
